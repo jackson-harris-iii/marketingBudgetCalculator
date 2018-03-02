06dc39c4160d229e65d4de539d6c944d8a819364
Digital Marketing <digitalmarketing@Squirrels-iMac.lan>
2018-03-01 17:18:09 -0800

    add contact form panel

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -6,20 +6,26 @@ $(document).ready( function () {
 function getStarted() {
     //begins question sequence
     console.log('getstarted')
-
+    //clears left panel contents
+    $('#welcomeDiv').remove()
+    //show contact form
+    $('#contactForm').show()
     //changes leftPanelTrigger button to nextButton
-    $('#leftPanelTrigger').text('Next').attr('id', 'nextButton')
-
+    $('#startButton').text('Next').attr('id', 'nextButton').attr('class', 'btn btn-outline-danger btn-lg mt-4')
+    var skip = $('<button>')
+    skip.text('Skip').attr('class', 'btn btn-outline-danger btn-lg mt-4').attr('id', 'skipButton')
+    $('#leftPanelStuff').append(skip)
 
 }
 
 function welcomePanels() {
+    $('#contactForm').hide()
     // changes leftPanel Trigger button to startbutton
     $('#leftPanelTrigger').text('Start').attr('id', 'startButton')
     console.log('welcome')
 
     //create welcome message
-    var starterItems = $('<div>')
+    var starterItems = $('<div>').attr('id', 'welcomeDiv')
     starterItems.append('<h1>Press Start To Begin</h1>')
     starterItems.append('<p>This Marketing Calculator will help you determine your marketing budget</p>')
     
@@ -28,19 +34,20 @@ function welcomePanels() {
 }
 
 function toolTip(infoField) {
-    console.log(target)
+    console.log(infoField)
 }
 
 function changeDisplay() {
     console.log('next item')
 }
 
+//default calculator data is displayed
+welcomePanels()
+
 //triggers next panel
 $('#nextButton').click(changeDisplay)
 
 $('#startButton').click(getStarted)
 //displays info about question when a user mouses over it.
-$('body').mouseover('.infoField', toolTip)
-//default calculator data is displayed
-welcomePanels()
+$('body').on('mouseover','.infoField', toolTip);
 })
