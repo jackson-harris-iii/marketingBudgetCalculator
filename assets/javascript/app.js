01acf73b32738fab51b2150d5fedfa45aa75fd55
Digital Marketing <digitalmarketing@Squirrels-iMac.lan>
2018-03-05 18:27:40 -0800

    fix next button, add questions form

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,25 +1,31 @@
 $(document).ready( function () {    
     console.log('ready')
 
-
+var contactCaptured = false;
 
 function getStarted() {
     //begins question sequence
     console.log('getstarted')
     //clears left panel contents
     $('#welcomeDiv').remove()
-    //show contact form
+    //show contact form if contact is not already captured
+    if (contactCaptured != true){
     $('#contactForm').show()
     //changes leftPanelTrigger button to nextButton
     $('#startButton').text('Next').attr('id', 'formSubmit').attr('class', 'btn btn-outline-danger btn-lg mt-4 nextButton')
     var skip = $('<button>')
     skip.text('Skip').attr('class', 'btn btn-outline-danger btn-lg mt-4').attr('id', 'skipButton')
     $('#leftPanelStuff').append(skip)
+    }
+    else {
+        questionsForm()
+    }
 
 }
 
 function welcomePanels() {
     $('#contactForm').hide()
+    $('#questionsForm').hide()
     // changes leftPanel Trigger button to startbutton
     $('#leftPanelTrigger').text('Start').attr('id', 'startButton')
     console.log('welcome')
@@ -37,23 +43,38 @@ function toolTip(infoField) {
     console.log(infoField)
 }
 
-function changeDisplay() {
+function questionsForm() {
     console.log('nextItem')
+    $('#contactForm').hide()
+    $('#questionsForm').show()
+
+    
 }
 
 function contactInfo() {
     var name = $('#contactName').val().trim()
+    var email = $('#contactEmail').val().trim()
+    var businessName = $('#businessName').val().trim()
+    var phone = $('#contactPhone').val().trim()
     console.log(name)
+    console.log(email)
+    console.log(businessName)
+    console.log(phone)
+    contactCaptured = true
+    questionsForm()
 }
 
 //default calculator data is displayed
 welcomePanels()
 
 //triggers next panel
-$('body').on('click', 'nextButton', changeDisplay)
-$('body', '#formSubmit', contactInfo)
+    $('body').on('click', '#skipButton', questionsForm)
 
-$('#startButton').click(getStarted)
+//collects contact information 
+$('body').on('click', '#formSubmit', contactInfo)
+
+//Loads contact collection form
+$('body').on('click', '#startButton' , getStarted)
 //displays info about question when a user mouses over it.
 $('body').on('mouseover','.infoField', toolTip);
 })
